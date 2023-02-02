728b8365d6afb83592dac9186ae95e4d1bb77efe
Ryo Hashioka <ryo.hashioka@gmail.com>
2023-02-02 20:37:44 +0900

    database

--- docker/public/database.php
+++ docker/public/database.php
@@ -7,14 +7,26 @@
 </head>
 
 <body>
+  <div>HOST: <?php echo $_ENV['DB_HOST'] ?? 'NO_NAME'; ?></div>
+  <div>NAME: <?php echo $_ENV['DB_NAME'] ?? 'NO_NAME'; ?></div>
+  <div>USER: <?php echo $_ENV['DB_USER'] ?? 'NO_NAME'; ?></div>
   <p>
-    <div>HOST: <?php echo $_ENV['DB_HOST'] ?? 'NO_NAME'; ?></div>
-    <div>NAME: <?php echo $_ENV['DB_NAME'] ?? 'NO_NAME'; ?></div>
-    <div>USER: <?php echo $_ENV['DB_USER'] ?? 'NO_NAME'; ?></div>
     <?php
+      if(isset($_ENV['DB_HOST']) && isset($_ENV['DB_NAME']) && isset($_ENV['DB_USER'])) {
+        $dsn = 'mysql:dbname=' . $_ENV['DB_NAME'] . ';host=' . $_ENV['DB_HOST'] . ';charset=utf8mb4';
+        $user = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASSWORD'];
 
-      // $pdo = new PDO($dsn, $user, $password);
-      echo 'データベースの接続に成功しました。';
+        try {
+          $pdo = new PDO($dsn, $user, $password);
+          echo 'データベースの接続に成功しました。';  
+        } catch (PDOException $e) {
+          echo 'データベース接続中にエラーが発生しました。';
+          echo '<div style="color:red;">' . $e->getMessage() . '</div>';
+        }
+      } else {
+        echo '接続情報が足りません。';
+      }
     ?>
   </p>
 </body>
